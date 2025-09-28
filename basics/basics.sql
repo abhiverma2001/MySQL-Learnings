e9657c1173b9c370e6b82fdec6ce91481d4d9145
Abhishek Chaudhary <abhishekchaudhary8856@gmail.com>
2025-09-28 10:58:34 +0530

    create database,table and insert records

--- basics/basics.sql
+++ basics/basics.sql
@@ -1,7 +1,11 @@
+
+##create the database name as companydb
 create database companydb;
 
+##to use this database using this query
 use companydb;
 
+##create a table name as customer with some fields
 create table customer(
 id int primary key,
 name varchar(20),
@@ -9,19 +13,24 @@ email varchar(25),
 address varchar(20)
 );
 
+##to check the table records
 select * from customer;
 
-insert into customer(id,name,email,address) values(2,'Ram','abhi12@gmail.com','Basti');
+##to insert some records int the customer table
+insert into customer(id,name,email,address) values(1,'Ram','abhi12@gmail.com','Basti');
 
+##then check again the data is inserted or not in the customer table
 select * from customer;
 
+##to delete the table name as customer
 drop table customer;
 
+##to delete the database also name as companydb
 drop database companydb;
 
+##the query used to check all databases exist or not
 show databases;
 
-create database comapnydb;
+drop database btm;
 
 show databases;
-
